c2b63cd915e29b5db0d6ec2f18d7d9dc6b0f68e1
Andrew <andrewmgunn26@gmail.com>
2021-10-02 17:24:09 -0500

    sd

--- Program.cs
+++ Program.cs
@@ -19,6 +19,93 @@ namespace SudentClubApp
         }
 
 
+        public void GetInput()
+        {
+            try
+            {
+                Console.Write("Enter Student First Name:");
+                string fName = Console.ReadLine();
+
+                Console.Write("Enter Student Last Name: ");
+                string lName = Console.ReadLine();
+
+                Console.Write("Enter Student Email: ");
+                string email = Console.ReadLine();
+
+                int id = Student.QueueId;
+
+                Student student = new Student(id, fName, lName, email);
+
+                Students.Add(student);
+            }
+            catch (Exception e)
+            {
+                logger.Error(e);
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public void ReadFromFile()
+        {
+            var id = 0;
+
+            try
+            {
+                if (!System.IO.File.Exists(fileName))
+                {
+                    this.WriteToFile();
+                }
+
+
+                StreamReader reader = new StreamReader(fileName);
+
+                while (!reader.EndOfStream)
+                {
+                    string[] lineData = reader.ReadLine().Split(",");
+                    Students.Add(new Student(Convert.ToInt32(lineData[0]), lineData[1], lineData[2], lineData[3]));
+                }
+
+                reader.Close();
+
+                foreach (Student s in Students)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                logger.Error(e);
+                throw;
+            }
+        }
+
+
+        public void WriteToFile()
+        {
+            try
+            {
+                StreamWriter writer = new StreamWriter(this.fileName, true);
+
+
+                foreach (Student s in Students)
+                {
+                    Student.QueueId++;
+                    writer.WriteLine($"{Student.QueueId}, {s.FirstName}, {s.LastName}, {s.Email}");
+                }
+
+                writer.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                this.logger.Error(e);
+                throw;
+            }
+        }
+
+
         public void DefaultPopulate()
         {
             Students = new List<Student>();
@@ -70,21 +157,58 @@ namespace SudentClubApp
             }
         }
 
+
+        public void FindStudent(string keyword)
+        {
+            try
+            {
+                Student s = Students.Find(student => student.FirstName.Equals(keyword));
+
+                Console.WriteLine(s);
+            }
+            catch (Exception e)
+            {
+                logger.Error(e);
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public void DeleteFoundStudent(string keyword)
+        {
+            try
+            {
+                List<Student> results;
+                Student student = null;
+                results = new List<Student>();
+                results = Students.FindAll(student => student.FirstName.Equals(keyword));
+
+                foreach (Student v in results)
+                {
+                    Students.Remove(v);
+                }
+                
+            }
+            catch (Exception e)
+            {
+                logger.Warn(e);
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
         public void DeleteStudent(string id)
         {
-
             Student s = Students.Find(student => student.FirstName == id);
 
             Console.WriteLine(Students.Remove(s));
-            
+
             //Console.WriteLine(s);
 
             foreach (var ss in Students)
             {
                 Console.WriteLine(ss);
             }
-
-
         }
 
         public void StudentsList2()
@@ -135,12 +259,18 @@ namespace SudentClubApp
         static void Main(string[] args)
         {
             Program p = new Program();
-            //p.logger.Error("error");
+            //p.GetInput();
+            //p.StudentsList();
+            //p.WriteToFile();
+            p.ReadFromFile();
 
-            //p.DefaultPopulate();
-            p.StudentsList2();
-            
-            p.DeleteStudent("Andy");
+            //Console.WriteLine("Find: ");
+            //string key = Console.ReadLine();
+            //p.FindStudent(key);
+            p.DeleteFoundStudent("andy");
+
+            p.StudentsList();
+            //p.WriteToFile();
         }
     }
 }
